8f4e2db6cd589459ea12d3658dbc5f7f0c69b9d7
Andrej <trac@desu.no-ip.info>
2011-06-12 22:51:26 +0200

    Nxt notes patchunpatch

--- Nxt.cpp
+++ Nxt.cpp
@@ -77,8 +77,17 @@ _cogl_setup ()
        But context->framebuffer_stack inited with an COGL_INVALID_HANDLE entry
       WARNING see "if (context->stub_winsys)" note about Clutter
        If winsys was stub_winsys it will create _cogl_onscreen_new and cogl_set_framebuffer
-    
-    
+    Cogl_set_default_context
+      Meh it's like already done by cogl_context_new
+    Cogl_onscreen_new
+      _cogl_framebuffer_init
+    Cogl_framebuffer_allocate
+      Nothing for offscreens, note says about cogl_offscreen_new_to_texture implicit allocation
+      onscreen_init vmethod
+        There's an if (onscreen->foreign_hwnd)
+          WARNING SKIPS creating window but not setting pixel format surely this is retard mode/bug, patched cogl not to setpixelformat
+        Else CreateWindowW, Get dummy_dc's pixel format, set the onscreen's pf to it
+        WARNING Cogl patched not to do that for Main, use unpatched for Nxt
     */
     
     cogl_renderer = cogl_renderer_new ();
