80206ec73d4c79e88b53f1fa1498ac5ad92a0e62
nOOb3167 <github@desu.no-ip.info>
2011-06-25 17:09:58 +0200

    Buildsystem fix

---
+++ Build/PathsCustomInclude.cmake
@@ -0,0 +1,11 @@
+IF(NOT DEFINED PCI_INCLUDED)
+MESSAGE("Including PathsCustomInclude.cmake")
+SET(PCI_INCLUDED "TRUE" CACHE INTERNAL "PathsCustomInclude inclusion guard")
+
+SET(PCI_PKG_CONFIG_EXECUTABLE "C:/testM/Glade/OBS/OBS24/mingw/bin/pkg-config.exe")
+SET(PCI_PKG_CONFIG_PATH "C:/testM/Glade/OBS/OBS24/mingw/lib/pkgconfig;C:/tmp2/clutter/cogl_build_004/lib/pkgconfig")
+
+SET(PCI_ALLEGRO_ROOT_DIR "C:/tmp2/AllegroX/V_5_0_2_1/inst")
+SET(PCI_ALLEGRO_DEBUG "TRUE")
+
+ENDIF()
