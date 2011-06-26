a7ea280234fb0b8091f0a4e165609070861408c3
nOOb3167 <github@desu.no-ip.info>
2011-06-26 23:41:25 +0200

    Buildsystem gob2

---
+++ Build/PathsCustomInclude.cmake
@@ -0,0 +1,13 @@
+IF(NOT DEFINED PCI_INCLUDED)
+MESSAGE("Including PathsCustomInclude.cmake")
+SET(PCI_INCLUDED "TRUE" CACHE INTERNAL "PathsCustomInclude inclusion guard")
+
+SET(PCI_PKG_CONFIG_EXECUTABLE "C:/testM/Glade/OBS/OBS24/mingw/bin/pkg-config.exe")
+SET(PCI_PKG_CONFIG_PATH "C:/testM/Glade/OBS/OBS24/mingw/lib/pkgconfig;C:/tmp2/clutter/cogl_build_004/lib/pkgconfig")
+
+SET(PCI_ALLEGRO_ROOT_DIR "C:/tmp2/AllegroX/V_5_0_2_1/inst")
+SET(PCI_ALLEGRO_DEBUG "TRUE")
+
+SET(PCI_GOB2_EXECUTABLE "C:\\tmp2\\glib\\gob2\\inst\\bin\\gob2.exe")
+
+ENDIF()
