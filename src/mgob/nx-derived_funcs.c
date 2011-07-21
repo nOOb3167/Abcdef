e292a630c5cf920e2f1057c220450e969ce214c7
nOOb3167 <github@desu.no-ip.info>
2011-07-21 03:27:35 +0200

    mgob2 moved new into _funcs.c

--- src/mgob/nx-derived_funcs.c
+++ src/mgob/nx-derived_funcs.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <src/mgob/nx-derived_funcs.h>
 
-void _nx_derived_something (NxDerived *self)
+NxDerived *
+nx_derived_new (void)
+{
+  return GET_NEW;
+}
+
+void
+_nx_derived_something (NxDerived *self)
 {
   printf ("_nx_derived_something\n");
 }
--- src/mgob/nx-derived_funcs.h
+++ src/mgob/nx-derived_funcs.h
@@ -1,9 +1,10 @@
 #ifndef NX_DERIVED_FUNCS_H_
 #define NX_DERIVED_FUNCS_H_
 
-//struct _NxDerived;
-//typedef struct _NxDerived NxDerived;
+NxDerived *
+nx_derived_new (void);
 
-void _nx_derived_something (NxDerived *self);
+void
+_nx_derived_something (NxDerived *self);
 
 #endif /* NX_DERIVED_FUNCS_H_ */
