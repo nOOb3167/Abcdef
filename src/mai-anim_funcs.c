cdeea1aa8f8854c15e94d697672b70de51a279a4
nOOb3167 <github@desu.no-ip.info>
2011-07-21 06:31:39 +0200

    MaiModel

--- src/mai-anim_funcs.c
+++ src/mai-anim_funcs.c
@@ -1,4 +1,5 @@
 #include <src/mai-node-anim.h>
+#include <src/mai-node-anim_funcs.h>
 #include <src/mai-anim_funcs.h>
 
 #define GET_NEW ((MaiAnim *)g_object_new(mai_anim_get_type(), NULL))
@@ -12,6 +13,13 @@ mai_anim_new_from (struct aiScene *scene, struct aiAnimation *from)
   self->duration = from->mDuration;
 
   self->channels = g_array_new (FALSE, TRUE, sizeof (MaiNodeAnim));
+  int cnt;
+  for (cnt=0; cnt<from->mNumChannels; ++cnt)
+    {
+      MaiNodeAnim *na;
+      na = mai_node_anim_new_from (scene, from->mChannels[cnt]);
+      g_array_append_vals (self->channels, na, 1);
+    }
 
   return self;
 }
---
+++ src/mai-model_funcs.c
@@ -0,0 +1,12 @@
+MaiModel *
+mai_model_new (void);
+
+MaiModel *
+mai_model_new (void)
+{
+  MaiModel *self = GET_NEW;
+
+
+
+  return self;
+}
