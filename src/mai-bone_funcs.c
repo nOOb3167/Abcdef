d1fad9e87f0e18244ec5475ed13d306e1d2eb61e
nOOb3167 <github@desu.no-ip.info>
2011-07-23 10:49:02 +0200

    mai_bone_new_from

--- src/mai-bone_funcs.c
+++ src/mai-bone_funcs.c
@@ -5,5 +5,19 @@ mai_bone_new_from (struct aiBone *bone)
 {
   MaiBone *self = GET_NEW;
 
+  self->name = g_strdup (bone->mName.data);
+  self->offset_matrix = g_new0 (CoglMatrix, 1);
+  ai_matrix_to_cogl_matrix (bone->mOffsetMatrix, self->offset_matrix);
+
+  self->weights = g_nx_vertex_weight_array_new ();
+  int cnt;
+  for (cnt=0; cnt<bone->mNumWeights; ++cnt)
+    {
+      NxVertexWeight nvw;
+      nvw.vertex_id = bone->mWeights[cnt].mVertexId;
+      nvw.weight = bone->mWeights[cnt].mWeight;
+      g_nx_vertex_weight_array_append_val (self->weights, &nvw);
+    }
+
   return self;
 }
