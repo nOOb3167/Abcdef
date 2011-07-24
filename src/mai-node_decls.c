3eedefd4981919b61af5929aa502f4cb0ec71db0
nOOb3167 <github@desu.no-ip.info>
2011-07-24 03:36:48 +0200

    mai-node_decls

---
+++ src/mai-node_decls.c
@@ -0,0 +1,3 @@
+#include <src/mai-node_decls.h>
+
+XPTR_ARRAY_MACRO_TYPE_DEFINE (mai_node, MaiNode);
---
+++ src/mai-node_decls.h
@@ -0,0 +1,8 @@
+#ifndef MAI_NODE_DECLS_H_
+#define MAI_NODE_DECLS_H_
+
+#include <src/array_macro.h>
+
+XPTR_ARRAY_MACRO_TYPE_DECLARE (mai_node, MaiNode);
+
+#endif /* MAI_NODE_DECLS_H_ */
