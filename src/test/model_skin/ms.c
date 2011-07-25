0bf3457e4c2319f61f1b99ec5208ae201c5d39ad
nOOb3167 <github@desu.no-ip.info>
2011-07-25 22:14:11 +0200

    ms towards using MaiAnimInstance

--- src/test/model_skin/ms.c
+++ src/test/model_skin/ms.c
@@ -11,6 +11,8 @@
 #include <src/gfx_lib_setup.h>
 #include <src/mai-model.h>
 #include <src/mai-model_funcs.h>
+#include <src/mai-node-anim.h>
+#include <src/mai-node-anim_funcs.h>
 #include <src/misc.h>
 #include <src/ai_example.h>
 
@@ -26,10 +28,13 @@ void
 _ms_stuff (MaiModel *mm);
 
 void
-nx_skin_transform (MaiModel *model, MaiAnimInstance *anim, MaiNode *mesh_node, GArray **verts_out);
+nx_skin_transform (MaiModel *model, MaiAnimInstance *anim_instance, MaiNode *mesh_node, GArray **verts_out);
 
 void
 nx_skin_transform_vert (CoglMatrix *tmtx, struct xvtx *vert_inout);
+
+void
+_nx_combine_keys (struct NxAnimKey pos, struct NxAnimKey rot, CoglMatrix *combined_inout);
 
 void
 _display_loop (void)
@@ -205,7 +210,7 @@ _ms_stuff (MaiModel *mm)
  * Should be taking an MaiAnimInstance anyway
  */
 void
-nx_skin_transform (MaiModel *model, MaiAnimInstance *anim, MaiNode *mesh_node, GArray **verts_out)
+nx_skin_transform (MaiModel *model, MaiAnimInstance *anim_instance, MaiNode *mesh_node, GArray **verts_out)
 {
   void acc_transform (MaiNode *node, CoglMatrix *acc_mtx)
   {
@@ -218,6 +223,37 @@ nx_skin_transform (MaiModel *model, MaiAnimInstance *anim, MaiNode *mesh_node, G
     cogl_matrix_multiply (acc_mtx, acc_mtx, node->transformation);
   }
 
+  void acc_transform_cur (MaiNode *node, CoglMatrix *leaf_replacement_mtx, CoglMatrix *acc_mtx)
+  {
+    void acc_transform_aux (gboolean first, MaiNode *node, CoglMatrix *leaf_replacement_mtx, CoglMatrix *acc_mtx)
+    {
+      CoglMatrix *node_transformation;
+      if (first)
+        node_transformation = leaf_replacement_mtx;
+      else
+        NULL == node ? NULL : (node_transformation = node->transformation);
+      if (node == NULL)
+        {
+          cogl_matrix_init_identity (acc_mtx);
+          return;
+        }
+      acc_transform_aux (0, node->parent, leaf_replacement_mtx, acc_mtx);
+      cogl_matrix_multiply (acc_mtx, acc_mtx, node_transformation);
+    }
+    acc_transform_aux (1, node, leaf_replacement_mtx, acc_mtx);
+  }
+
+  MaiAnim *anim;
+  anim = anim_instance->anim;
+
+  MaiNodeAnim *na;
+  na = g_mai_node_anim_ptr_array_index (anim->channels, 0);
+  g_xassert (0 == g_strcmp0 (na->node_name, "Bone"));
+  struct NxAnimKey cur_pos_key;
+  struct NxAnimKey cur_rot_key;
+  cur_pos_key = g_array_index (na->position_keys, struct NxAnimKey, anim_instance->current_frame);
+  cur_rot_key = g_array_index (na->rotation_keys, struct NxAnimKey, anim_instance->current_frame);
+
   MaiBone *bone;
   MaiNode *bone_node;
 
@@ -227,9 +263,13 @@ nx_skin_transform (MaiModel *model, MaiAnimInstance *anim, MaiNode *mesh_node, G
   g_xassert (bone_node);
 
   CoglMatrix bone_ws;
+  CoglMatrix cur_bone;
+  CoglMatrix cur_bone_ws;
   CoglMatrix mesh_node_ws;
   CoglMatrix mesh_node_ws_inv;
 
+  _nx_combine_keys (cur_pos_key, cur_rot_key, &cur_bone);
+  acc_transform_cur (bone_node, &cur_bone, &cur_bone_ws);
   acc_transform (bone_node, &bone_ws);
   acc_transform (mesh_node, &mesh_node_ws);
   nx_cogl_matrix_get_inverse (&mesh_node_ws, &mesh_node_ws_inv);
@@ -237,7 +277,7 @@ nx_skin_transform (MaiModel *model, MaiAnimInstance *anim, MaiNode *mesh_node, G
   CoglMatrix tmtx;
   cogl_matrix_init_identity (&tmtx);
   cogl_matrix_multiply (&tmtx, &tmtx, &mesh_node_ws_inv);
-  cogl_matrix_multiply (&tmtx, &tmtx, &bone_ws);
+  cogl_matrix_multiply (&tmtx, &tmtx, &cur_bone_ws);
   cogl_matrix_multiply (&tmtx, &tmtx, bone->offset_matrix);
 
   GArray *new_verts;
@@ -261,4 +301,22 @@ nx_skin_transform_vert (CoglMatrix *tmtx, struct xvtx *vert_inout)
   float vert_f[4] = {vert_inout->x, vert_inout->y, vert_inout->z, 1.0f};
   cogl_matrix_transform_point (tmtx, &vert_f[0], &vert_f[1], &vert_f[2], &vert_f[3]);
   vert_inout->x = vert_f[0]; vert_inout->y = vert_f[1]; vert_inout->z = vert_f[2];
+}
+
+void
+_nx_combine_keys (struct NxAnimKey pos, struct NxAnimKey rot, CoglMatrix *combined_inout)
+{
+  CoglMatrix cur_bone_mtx;
+  cogl_matrix_init_identity (&cur_bone_mtx);
+  cogl_matrix_translate (&cur_bone_mtx, pos.val.vec.x, pos.val.vec.y, pos.val.vec.z);
+  CoglQuaternion quat;
+  float rotang;
+  cogl_quaternion_init_identity (&quat);
+  quat.w = rot.val.rot.w; quat.x = rot.val.rot.x;
+  quat.y = rot.val.rot.y; quat.z = rot.val.rot.z;
+  struct xvtx vec3;
+  nx_cogl_quaternion_to_rotation_axis_and_angle (&quat, &rotang, &vec3);
+  cogl_matrix_rotate (&cur_bone_mtx, rotang, vec3.x, vec3.y, vec3.z);
+
+  *combined_inout = cur_bone_mtx;
 }
