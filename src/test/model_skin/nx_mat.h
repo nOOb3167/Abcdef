bd74ffbbc1f3334af93d2815cbdf1e860135193b
nOOb3167 <github@desu.no-ip.info>
2011-08-04 06:42:46 +0200

    sr project

--- src/test/model_skin/nx_mat.h
+++ src/test/model_skin/nx_mat.h
@@ -14,12 +14,24 @@ struct NxMat
 typedef struct NxMat NxMat;
 
 #define NX_MAT_ELT(m,r,c) \
-  ((m)->vals[(4*(r))+(c)])
+  ((m)->vals[(4*(c))+(r)])
 
 void
 nx_mat_init_identity (NxMat *what);
 
 void
 nx_mat_multiply (NxMat *result, NxMat *a, NxMat *b);
+
+void
+nx_mat_translation (NxMat *what, float x, float y, float z);
+
+void
+nx_mat_scale (NxMat *what, float x, float y, float z);
+
+void
+nx_mat_projection (NxMat *what, float near);
+
+void
+nx_mat_transform (NxMat *what, NxVec4 *vec_inout);
 
 #endif /* NX_MAT_H_ */
--- src/test/model_skin/sr.c
+++ src/test/model_skin/sr.c
@@ -8,6 +8,20 @@
 
 #include <glib.h>
 #include <src/error.h>
+#include <nx_mat.h>
+
+void
+sr_project_one (NxMat *mst, NxVec4 *vec_inout)
+{
+  NxVec4 temp;
+  temp = *vec_inout;
+  nx_mat_transform (mst, &temp);
+  temp.vals[0] /= temp.vals[3];
+  temp.vals[1] /= temp.vals[3];
+  temp.vals[2] /= temp.vals[3];
+  temp.vals[3] = 1.0f;
+  *vec_inout = temp;
+}
 
 int
 main (int argc, char **argv)
@@ -31,6 +45,12 @@ main (int argc, char **argv)
   al_set_target_backbuffer (display);
 
   al_draw_bitmap (bmp, 20, 20, 0);
+
+  NxMat p_mat;
+  nx_mat_projection (&p_mat, 1.0f);
+  NxVec4 vec = {1.0f, 1.0f, -1.0f, 1.0f};
+
+  sr_project_one (&p_mat, &vec);
 
   al_flip_display ();
 
