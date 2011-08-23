4468f437f3411276c3f4d3e46171ee41bb5d9b79
nOOb3167 <github@desu.no-ip.info>
2011-08-24 00:53:48 +0200

    sr split main off to sr_main

--- src/test/model_skin/sr.c
+++ src/test/model_skin/sr.c
@@ -1,10 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_windows.h>
-#include <allegro5/allegro_opengl.h>
-#include <allegro5/allegro_primitives.h>
 
 #include <glib.h>
 #include <src/error.h>
@@ -585,109 +580,6 @@ sr_skeletal_anim (MaiModel *model,
   sr_vertex_transform_calculate (mesh_node, vbmap, name_bone_mtx_map, &trans_verts);
 
   *trans_verts_out = trans_verts;
-}
-
-int
-main (int argc, char **argv)
-{
-  ALLEGRO_DISPLAY *display;
-
-  g_type_init ();
-
-  /**
-   * Initialize g_state
-   */
-  struct NxState state = {0};
-  g_state = g_new0 (struct NxState, 1);
-  *g_state = state;
-
-  NxMat z_mat;
-
-  nx_mat_projection (&z_mat, -1.0f);
-  //nx_mat_translation (&z_mat, 0.0f, 0.0f, -3.0f);
-  //NX_MAT_ELT (&z_mat, 2, 3) = -3.0f;
-
-  /**
-   * Experimental findings:
-   *   For the identity ortho matrix to match blender's
-   *   Z up, Y towards, X right, X has to be inverted, Y has to be inverted.
-   *   But why is the translation on positive Z?
-   */
-  /*
-  nx_mat_ortho (&z_mat);
-  nx_mat_scale (&z_mat, -1.0f, -1.0f, 1.0f);
-  nx_mat_translation (&z_mat, 0.0f, 0.0f, 3.0f);
-  */
-
-  //nx_mat_translation (&z_mat, 0.0f, 0.0f, -3.0f);
-  g_state->p_mat = z_mat;
-  g_state->w_mat = z_mat;
-
-  al_init ();
-  al_init_primitives_addon ();
-
-  al_install_keyboard ();
-  al_install_mouse ();
-
-  display = al_create_display (100, 100);
-  g_xassert (display);
-
-  al_set_target_backbuffer (display);
-
-  MaiModel *model;
-  model = mai_model_new_from_file ("c_sr_weight.dae");
-  MaiNode *mesh_node;
-  mesh_node = g_hash_table_lookup (model->name_node_map, "Cube");
-  g_xassert (mesh_node);
-
-  MaiAnimInstance *mai;
-  g_xassert (model->anims->len > 0);
-  mai = mai_anim_instance_new_from_anim (
-                                         g_mai_anim_ptr_array_index (model->anims, 0),
-                                         model->name_node_map,
-                                         model->nodes);
-
-  sr_weight_dump (model);
-
-  struct SrNodeGraph *sr_model;
-  sr_node_graph_from_model (model, &sr_model);
-
-//  struct SrNodeGraph *aux_sr_model;
-//  sr_node_graph_copy (&aux_sr_model, sr_model);
-//
-//  GArray *trans_verts;
-//  sr_skeletal_anim (model, mai, mesh_node, aux_sr_model, &trans_verts);
-
-  ALLEGRO_KEYBOARD_STATE aks;
-
-  int frame;
-  for (frame=0; frame<60; ++frame)
-    {
-      al_get_keyboard_state (&aks);
-      sr_update_global_ypr (&aks);
-      al_clear_to_color (al_map_rgb (0, 0, 0));
-
-      struct SrNodeGraph *aux_sr_model;
-      sr_node_graph_copy (&aux_sr_model, sr_model);
-
-      GArray *trans_verts;
-      sr_skeletal_anim (model, mai, mesh_node, aux_sr_model, &trans_verts);
-
-      mai->current_frame += mai->current_frame >= 30 ? -30 : 1;
-
-      sr_skeletal_draw_node_trans (&g_state->w_mat, aux_sr_model, mesh_node, trans_verts);
-
-      NxVec4 uvecs[2] = {{0.0f, 0.0f, 0.0f, 1.0f}, {3.0f, 3.0f, 0.0f, 1.0f}};
-      NxMat uvmat;
-      uvmat = g_state->w_mat;
-      nx_mat_translation (&uvmat, 0.0f, 0.0f, -3.0f);
-      sr_draw_unit_vec_at (&uvmat, &uvecs[0], &uvecs[1]);
-
-      al_flip_display ();
-      al_rest (0.05f);
-    }
-
-  return EXIT_SUCCESS;
 }
 
 struct SrNode *
--- src/test/model_skin/sr.h
+++ src/test/model_skin/sr.h
@@ -1,7 +1,16 @@
 #ifndef SR_H_
 #define SR_H_
 
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_windows.h>
+#include <allegro5/allegro_opengl.h>
+#include <allegro5/allegro_primitives.h>
+#include <src/cogl_include.h>
 #include <glib.h>
+#include <src/mai-model.h>
+#include <src/mai-model_funcs.h>
+#include <src/mai-node-anim.h>
+#include <src/mai-node-anim_funcs.h>
 #include <nx_mat.h>
 
 struct NxState
@@ -43,14 +52,84 @@ struct SrNodeGraph
 };
 
 void
-sr_node_graph_copy (struct SrNodeGraph **result, struct SrNodeGraph *what);
+nx_mat_from_cogl_matrix (NxMat *mat, CoglMatrix *cogl_matrix);
 
 void
-nx_mat_from_cogl_matrix (NxMat *mat, CoglMatrix *cogl_matrix);
+sr_project_one (NxMat *mst, NxVec4 *vec_inout);
+
+void
+sr_draw_unit_vec_at (NxMat *mst, NxVec4 *pos, NxVec4 *dir);
+
+void
+sr_draw_tri (NxMat *mst, NxVec4 pts[3]);
+
+void
+sr_draw_node (NxMat *mst, GArray *verts, GArray *indices, GArray *uvs);
+
+void
+sr_skeletal_draw_node_trans (NxMat *mst,
+                             struct SrNodeGraph *sr_model,
+                             MaiNode *mesh_node,
+                             GArray *verts);
+
+void
+sr_update_global_ypr (ALLEGRO_KEYBOARD_STATE *aks);
+
+struct SrNode *
+_sr_node_walk (struct SrNodeGraph *mdl, MaiNode *nde_foreign);
+
+void
+sr_model_from_mai_model (struct SrNodeGraph **result, MaiModel *model);
+
+void
+sr_weight_dump (MaiModel *model);
+
+void
+sr_update_node_graph (MaiAnimInstance *mai, struct SrNodeGraph *graph);
 
 void
 sr_node_accumulate (struct SrNodeGraph *sr_model,
                     struct SrNode *src,
                     NxMat *result);
+
+void
+sr_bone_matrix (struct SrNodeGraph *sr_model,
+                struct SrNode *mesh_node,
+                MaiBone *bone,
+                NxMat *result_out);
+
+void
+sr_bone_matrices (struct SrNodeGraph *sr_model,
+                  struct SrNode *mesh_node_sr,
+                  MaiNode *mesh_node,
+                  GHashTable **nbm_out);
+
+void
+sr_vertex_bones (MaiNode *mesh_node, GPtrArray **vbmap_out);
+
+void
+sr_vertex_transform_calculate (MaiNode *mesh_node,
+                               GPtrArray *vbmap,
+                               GHashTable *name_bone_mtx_map,
+                               GArray **trans_verts_out);
+
+void
+sr_node_graph_from_model (MaiModel *model,
+                          struct SrNodeGraph **sr_model_out);
+
+void
+sr_skeletal_anim (MaiModel *model,
+                  MaiAnimInstance *mai,
+                  MaiNode *mesh_node,
+                  struct SrNodeGraph *sr_model_io,
+                  GArray **trans_verts_out);
+
+struct SrNode *
+_sr_copy_node_walk (struct SrNodeGraph *res_mdl,
+                    struct SrNodeGraph *orig_mdl,
+                    struct SrNode *nde);
+
+void
+sr_node_graph_copy (struct SrNodeGraph **result, struct SrNodeGraph *what);
 
 #endif /* SR_H_ */
