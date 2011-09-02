7af43120c7f2f388b960918e6d1115d683f57c39
nOOb3167 <github@desu.no-ip.info>
2011-09-02 05:47:56 +0200

    test_gvalue

--- src/mgob/test_gvalue.c
+++ src/mgob/test_gvalue.c
@@ -33,6 +33,29 @@ nx_value_object_hash (gconstpointer v)
 gboolean
 nx_value_object_equal (gconstpointer v1, gconstpointer v2)
 {
+  g_xassert (G_IS_VALUE (v1));
+  g_xassert (G_IS_VALUE (v2));
+
+  if (!g_value_type_compatible (G_VALUE_TYPE (v1),
+                                G_VALUE_TYPE (v2)));
+    {
+      g_xassert (FALSE);
+
+      return 0;
+    }
+
+  if (G_VALUE_HOLDS_INT (v1) && G_VALUE_HOLDS_INT (v2))
+    {
+      return g_value_get_int (v1) == g_value_get_int (v2);
+    }
+
+  if (G_VALUE_HOLDS_OBJECT (v1) && G_VALUE_HOLDS_OBJECT (v2))
+    {
+      return g_value_get_object (v1) == g_value_get_object (v2);
+    }
+
+  g_xassert (FALSE);
+
   return v1 == v2;
 }
 
@@ -46,6 +69,36 @@ nx_value_free (gpointer v)
   g_free (v);
 }
 
+void
+nx_g_value_new (GValue **v, GType t)
+{
+  *v = g_malloc (sizeof (**v));
+  **v = nx_empty_value;
+  g_value_init (*v, t);
+}
+
+GValue *
+nx_box_object (GObject *obj)
+{
+  GValue *ret;
+
+  nx_g_value_new (&ret, G_TYPE_OBJECT);
+  g_value_set_object (ret, obj);
+
+  return ret;
+}
+
+GObject *
+nx_unbox_object (GValue *v)
+{
+  GObject *ret;
+
+  g_xassert (G_VALUE_HOLDS_OBJECT (v));
+  ret = g_value_get_object (v);
+
+  return ret;
+}
+
 int
 main (int argc, char **argv)
 {
@@ -54,12 +107,12 @@ main (int argc, char **argv)
   GObject *obj;
   obj = g_object_new (G_TYPE_OBJECT, NULL);
 
+  GObject *obj_nothere;
+  obj_nothere = g_object_new (G_TYPE_OBJECT, NULL);
+
+
   MgobX1 *x1;
   x1 = mgob_x1_new ("x1");
-
-  GValue val = NX_VALUE_INIT;
-  g_value_init (&val, G_TYPE_OBJECT);
-  g_value_set_object (&val, obj);
 
   GHashTable *ht;
   ht = g_hash_table_new_full (nx_value_object_hash,
@@ -68,15 +121,11 @@ main (int argc, char **argv)
                               nx_value_free);
 
   GValue *k1;
-  k1 = g_malloc (sizeof (*k1));
-  *k1 = nx_empty_value;
-  g_value_init (k1, G_TYPE_OBJECT);
+  nx_g_value_new (&k1, G_TYPE_OBJECT);
   g_value_set_object (k1, G_OBJECT (obj));
 
   GValue *k2;
-  k2 = g_malloc (sizeof (*k2));
-  *k2 = nx_empty_value;
-  g_value_init (k2, G_TYPE_INT);
+  nx_g_value_new (&k2, G_TYPE_INT);
   g_value_set_int (k2, 1234);
 
   /**
@@ -85,12 +134,18 @@ main (int argc, char **argv)
    * get the fundamental type covered.
    */
   GValue *v1;
-  v1 = g_malloc (sizeof (*v1));
-  *v1 = nx_empty_value;
-  g_value_init (v1, G_TYPE_OBJECT);
+  nx_g_value_new (&v1, G_TYPE_OBJECT);
   g_value_set_object (v1, G_OBJECT (x1));
 
   g_hash_table_insert (ht, k2, v1);
+
+  GValue *nothere;
+  nx_g_value_new (&nothere, G_TYPE_OBJECT);
+  g_value_set_object (nothere, obj_nothere);
+
+  MgobX1 *ok2o;
+  ok2o = MGOB_X1 (g_value_get_object (g_hash_table_lookup (ht, nothere)));
+  g_xassert (!ok2o);
 
   g_object_unref (obj);
   g_object_unref (x1);
