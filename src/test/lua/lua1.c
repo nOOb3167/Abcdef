56b8b071bc4f24376fde07a3967225b532ed3c49
nOOb3167 <github@desu.no-ip.info>
2011-10-20 05:53:19 +0200

    lua yield across CFunction boundary

--- src/test/lua/lua1.c
+++ src/test/lua/lua1.c
@@ -1,13 +1,49 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <lua.h>
 #include <lauxlib.h>
+
+int
+f1_c (lua_State *L)
+{
+  printf ("Continuation\n");
+  return 0;
+}
+
+int
+f1 (lua_State *L)
+{
+  printf ("DERP\n");
+  return lua_yieldk (L, 0, 1234, f1_c);;
+}
 
 int
 main (int argc, char **argv)
 {
   lua_State *L;
+  lua_State *L2;
+  int ret;
 
   L = luaL_newstate ();
+  L2 = lua_newthread (L);
+
+  lua_pushcfunction (L2, f1);
+  ret = lua_resume (L2, 0);
+
+  if (LUA_YIELD != ret)
+    {
+      printf ("FAIL |%s|\n", lua_tostring (L2, -1));
+      return EXIT_FAILURE;
+    }
+
+  ret = lua_resume (L2, 0);
+
+  if (LUA_OK != ret)
+    {
+      printf ("FAIL |%s|\n", lua_tostring (L2, -1));
+      return EXIT_FAILURE;
+    }
+
 
   return EXIT_SUCCESS;
 }
